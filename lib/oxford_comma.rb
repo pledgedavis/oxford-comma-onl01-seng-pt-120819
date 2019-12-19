893176e24de5901b729f584e5ac1e0c88a1e4aaa
pledgedavis <pledgedavis14@gmail.com>
2019-12-19 23:11:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,11 @@
 def oxford_comma(array)
-# counter = 0
-# hello =""
+ counter = 0
+ hello =""
 if counter < array.length 
    hello << array[counter]
 counter+=1
   elsif array.length == 2 
-    return array.join(" and ")
- 
-  elsif array.length > 2
-    array.insert(-2, "and ")
-    return array.join(", ")
+   
   end 
   hello
 end
