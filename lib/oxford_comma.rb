6feb7e713f7bb2e57f4f7e476763a409f5251f73
pledgedavis <pledgedavis14@gmail.com>
2019-12-20 00:12:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,38 @@
 def oxford_comma(array)
+if array.length == 1
+array.join
+elsif array.length == 2
+array.join(" and ")
+else
+last = array.pop
+array.join(", ") +", and "+ last
 
+  end
 end
+
+
+
+
+
+# counter = 0
+# hello =""
+# while counter < array.length 
+#   hello << array[counter]
+# counter+=1
+#   end 
+#   hello
+# end
+
+
+
+#   def oxford_comma
+# ["hello","goodbye"].to_sentence
+# end
+
+
+
+
+
+
+
+
